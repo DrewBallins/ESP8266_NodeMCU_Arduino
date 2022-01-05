65b2036340e4003f2ad29de10a670568ab9ebff6
Andrew Wild <ajwild1225@gmail.com>
2022-01-04 21:04:07 -0600

    mid-work save of udp testing

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,14 @@
  *****************************/
 #include <Arduino.h>
 #include "DHT.h"
+#include <ESP8266WiFi.h>
+#include "ESPAsyncUDP.h"
 
 /******************************
  * Defines
  *****************************/
 #define TWO_SEC_DELAY      2000
+#define NUM_DATA           4
 #define DHT_PIN            D1
 #define DHT_TYPE           DHT11
 //#define DHT_TYPE         DHT22
@@ -17,7 +20,8 @@
  * Local Function Declarations
  ******************************/
 static void calculate_dht_data(void);
-static void display_dht_data(void);
+static void display_dht_data_serial(void);
+static void send_dht_data_udp(void);
 
 /*******************************
  * Initialize DHT Sensor
@@ -26,16 +30,44 @@ DHT dht(DHT_PIN, DHT_TYPE);
 float dht_humidity;
 float dht_temperature;
 
+/*******************************
+ * Initialize UDP
+ ******************************/
+const char * ssid = ***REDACTED***
+const char * password = ***REDACTED***
+AsyncUDP udp;
+IPAddress nodemcuv2_IP_address = IPAddress(192,168,1,100);
+IPAddress rpi_IP_address = IPAddress(192,168,1,14);
+uint16_t udp_port = 42069;
+
 void setup() {
    Serial.begin(115200);
    dht.begin();
+
+   WiFi.mode(WIFI_STA);
+   WiFi.begin(ssid, password);
+   if (WiFi.waitForConnectResult() != WL_CONNECTED)
+   {
+      Serial.println("WiFi Failed");
+      while (true)
+      {
+         delay(1000);
+      }
+   }
+   if (udp.connect(nodemcuv2_IP_address, udp_port))
+   {
+      Serial.println("UDP connected!");
+      //Send unicast
+      udp.print("Hello Server!");
+   }
 }
 
 void loop() {
    delay(TWO_SEC_DELAY); // wait for two seconds between DHT measurements   
    
    calculate_dht_data();
-   display_dht_data();
+   display_dht_data_serial();
+   send_dht_data_udp();
 }
 
 /*******************************
@@ -47,7 +79,7 @@ static void calculate_dht_data(void)
    dht_temperature = dht.readTemperature();
 }
 
-static void display_dht_data(void)
+static void display_dht_data_serial(void)
 {
    Serial.print("DHT Humidity = ");
    Serial.print(dht_humidity);
@@ -55,4 +87,24 @@ static void display_dht_data(void)
    Serial.print("DHT Temperature = ");
    Serial.print(dht_temperature);
    Serial.println("C");   
+}
+
+static void send_dht_data_udp(void)
+{
+   // first we need to pack dht data into uint8_t buffer array
+   uint8_t dht_data_packed[NUM_DATA] = 
+   {
+      (uint8_t)dht_humidity,  // integer value
+      (uint8_t)((dht_humidity - (uint8_t)dht_humidity) * 100), // decimal value
+      (uint8_t)dht_temperature,  // integer value
+      (uint8_t)((dht_temperature - (uint8_t)dht_temperature) * 100)  // decimal value
+   };
+
+   // next we create a message object and write our data to it
+   AsyncUDPMessage dht_data_message = AsyncUDPMessage(sizeof(dht_data_packed));
+   dht_data_message.write(dht_data_packed, sizeof(dht_data_packed));
+
+   // finally we send the udp packet with packed data to rpi for processing
+   udp.broadcastTo(dht_data_message, udp_port);
+   udp.sendTo(dht_data_message, rpi_IP_address, udp_port);
 }
