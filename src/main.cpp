bc28b75085ce12c5e84e05e6ad796e0cc666f19f
Andrew Wild <ajwild1225@gmail.com>
2022-01-11 20:07:26 -0600

    added macro for serial debug

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@
 #define DHT_TYPE           DHT11
 //#define DHT_TYPE         DHT22
 //#define DHT_TYPE         DHT21
+//#define SERIAL_DEBUG     // uncomment this #define to enable debug prints over serial
 
 /*******************************
  * Local Function Declarations
@@ -48,7 +49,9 @@ void setup() {
    WiFi.begin(ssid, password);
    if (WiFi.waitForConnectResult() != WL_CONNECTED)
    {
+   #ifdef SERIAL_DEBUG      
       Serial.println("WiFi Failed");
+   #endif
       while (true)
       {
          delay(1000);
@@ -56,9 +59,9 @@ void setup() {
    }
    if (udp.connect(nodemcuv2_IP_address, udp_port))
    {
+   #ifdef SERIAL_DEBUG
       Serial.println("UDP connected!");
-      //Send unicast
-      udp.print("Hello Server!");
+   #endif
    }
 }
 
@@ -66,7 +69,9 @@ void loop() {
    delay(TWO_SEC_DELAY); // wait for two seconds between DHT measurements   
    
    calculate_dht_data();
+#ifdef SERIAL_DEBUG
    display_dht_data_serial();
+#endif
    send_dht_data_udp();
 }
 
@@ -105,6 +110,11 @@ static void send_dht_data_udp(void)
    dht_data_message.write(dht_data_packed, sizeof(dht_data_packed));
 
    // finally we send the udp packet with packed data to rpi for processing
-   udp.broadcastTo(dht_data_message, udp_port);             // TODO: why does this work
-   udp.sendTo(dht_data_message, rpi_IP_address, udp_port);  // but this doesn't??
+   size_t send_result = udp.sendTo(dht_data_message, rpi_IP_address, udp_port);
+
+#ifdef SERIAL_DEBUG
+   // print out error message (if any) or length
+   Serial.print("send result = ");
+   Serial.println(send_result);
+#endif
 }
