e44cc4ccb1c19139c0033c750dbb243148b2a57b
Andrew Wild <ajwild1225@gmail.com>
2022-01-14 21:39:54 -0600

    slight setup changes + more debug

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,6 @@ float dht_temperature;
 const char * ssid = ***REDACTED***
 const char * password = ***REDACTED***
 AsyncUDP udp;
-IPAddress nodemcuv2_IP_address = IPAddress(192,168,1,100);
 IPAddress rpi_IP_address = IPAddress(192,168,1,14);
 uint16_t udp_port = 42069;
 
@@ -59,11 +58,11 @@ void setup() {
          delay(1000);
       }
    }
-   if (udp.connect(nodemcuv2_IP_address, udp_port))
+   else
    {
-   #ifdef SERIAL_DEBUG
-      Serial.println("UDP connected!");
-   #endif
+   #ifdef SERIAL_DEBUG      
+      Serial.println("WiFi Success");
+   #endif      
    }
 }
 
@@ -111,7 +110,11 @@ static void send_dht_data_udp(void)
 
    // next we create a message object and write our data to it
    AsyncUDPMessage dht_data_message = AsyncUDPMessage(sizeof(dht_data_packed));
-   dht_data_message.write(dht_data_packed, sizeof(dht_data_packed));
+#ifdef SERIAL_DEBUG
+   size_t pack_result = dht_data_message.write(dht_data_packed, sizeof(dht_data_packed));
+#else
+   (void)dht_data_message.write(dht_data_packed, sizeof(dht_data_packed));
+#endif
 
    // finally we send the udp packet with packed data to rpi for processing
 #ifdef SERIAL_DEBUG
@@ -121,6 +124,28 @@ static void send_dht_data_udp(void)
 #endif
 
 #ifdef SERIAL_DEBUG
+   // print out length of packed data
+   Serial.print("dacked data length = ");
+   Serial.println(pack_result);
+   
+   // print out packed data
+   Serial.print("packed data = ");
+   for (uint8_t data_index = 0; data_index < NUM_DATA; data_index++)
+   {
+      Serial.print(dht_data_packed[data_index]);
+      Serial.print(";");
+   }
+   Serial.println();
+   
+   // print out udp message
+   Serial.print("dht_data_message data = ");
+   for (uint8_t data_index = 0; data_index < NUM_DATA; data_index++)
+   {
+      Serial.print(dht_data_message.data()[data_index]);
+   }
+   Serial.print(", length = ");
+   Serial.println(dht_data_message.length());
+
    // print out error message (if any) or length
    Serial.print("send result = ");
    Serial.println(send_result);
